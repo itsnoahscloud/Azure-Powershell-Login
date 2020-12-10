3562d086b349bc4222af3d056afc1a90df8c79da
itsnoahscloud <74538930+itsnoahscloud@users.noreply.github.com>
2020-12-10 15:51:59 +1000

    Create Azure-Powershell-and-az-Login.ps1
    This is how to login in Azure using Powershell and az.

---
+++ Azure-Powershell-and-az-Login.ps1
@@ -0,0 +1,26 @@
+# Login to Azure using Powershell and az
+$adminUPN="MyEmailAddress@domain.com"
+
+$userCredential = Get-Credential -UserName $adminUPN -Message "Type the password."
+
+connect-msolservice -Credential $userCredential
+
+connect-azaccount
+
+az login --tenant 'abcd1234-ab12-1234-ab12-123456abcdef'
+
+
+# Check PowerShell Subscription
+get-azsubscription
+
+# Check Azure Accounts
+az account list --output table
+
+# Optional: Clear accounts if needed
+az account clear
+
+# Optional: Set PowerShell Subscription to default
+Set-AzContext -SubscriptionId "xxxx-xxxx-xxxx-xxxx"
+
+# Optional: Set az Subscription to True (Set to default)
+az account set --subscription "xxxx-xxxx-xxxx-xxxx"
